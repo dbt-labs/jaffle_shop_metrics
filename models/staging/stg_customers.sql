b59de056c248659f4e2d4217e252e4d66b589ef3
Mirna Wong <mirna.wong@fishtownanalytics.com>
2022-07-28 13:39:57 +0000

    test

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -4,11 +4,11 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_customers') }}
+    select * from {{ ref(‘raw_customers’) }}
 
 ),
 
-renamed test
+renamed as (
 
     select
         id as customer_id,
