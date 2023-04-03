2efbcbf7844a91c069dd3218c00bcd13b8a9e006
Biswanath Bal <biswanath.x.bal@gsk.com>
2023-04-03 17:08:00 +0000

    changes view to table (Order)

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,23 +1,13 @@
-with orders as (
+{{
+    config(
+        materialized = 'table',
+        tags=['retail']
+    )
+}}
+with
+    orders as (select * from {{ ref("int_order_payments_pivoted") }}),
+    customers as (select * from {{ ref("int_customer_order_history_joined") }}),
+    final as (select * from orders left join customers using (customer_id))
 
-    select * from {{ ref('int_order_payments_pivoted') }}
-
-)
-,
-customers as (
-
-    select * from {{ ref('int_customer_order_history_joined') }}
-
-)
-,
-final as (
-
-    select 
-        *
-    from orders 
-    left join customers using (customer_id)
-
-)
-
-select * from final
-
+select *
+from final
